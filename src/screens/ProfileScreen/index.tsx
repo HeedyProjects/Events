f79e586a7415c050b7df40322fb259c60abb7b7d
Veronika <trotsenko_nika@mail.ru>
2022-06-01 11:49:06 +0200

    ProfileScreen in Progress

--- src/screens/ProfileScreen/index.tsx
+++ src/screens/ProfileScreen/index.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet} from 'react-native';
 import CustomText from '../../components/CustomText';
+import Head from '../ProfileScreen/Head';
+import Info from '../ProfileScreen/Info';
+import Notifications from '../ProfileScreen/Notifications';
+import Link from '../ProfileScreen/Link';
+const title = 'Профиль';
+
 export default function Events() {
   return (
-    <View>
-      <CustomText textType="SFProDisplayBold">
-        There will be Profile Screen
+    <View style={styles.container}>
+      <Head />
+      <CustomText textType="SFProDisplayBlack" style={styles.title}>
+        {title}
       </CustomText>
+      <View style={styles.frame}>
+        <Info />
+      </View>
+      <Notifications />
+      <Link />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+  title: {
+    fontWeight: 'bold',
+    fontSize: 28,
+    margin: 10,
+    marginTop: 20,
+    marginLeft: 12,
+  },
+
+  frame: {
+    borderColor: 'lightgrey',
+    borderRadius: 8,
+    borderWidth: 1,
+    marginLeft: 12,
+    marginRight: 12,
+    margin: 10,
+    padding: 46,
+  },
+});

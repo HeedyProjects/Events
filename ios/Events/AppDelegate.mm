e6504e601cfa3ec880bd913fc724a59a2ad18be2
Dmitry Tsyplov <dmitry.tsyplov@mediapark.com>
2022-06-01 23:28:11 +0200

    comm

--- ios/Events/AppDelegate.mm
+++ ios/Events/AppDelegate.mm
@@ -57,7 +57,7 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  [RNSplashScreen show]; 
+  [RNSplashScreen show];
   return YES;
 }
 

49d3cb109cd7a69f3de575d82663324345b0e9d5
Veronika <trotsenko_nika@mail.ru>
2022-06-22 11:23:14 +0200

    Button Add

---
+++ src/screens/Registration/Button.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {StyleSheet, TouchableOpacity} from 'react-native';
+import CustomText from '../../components/CustomText';
+import colors from '../../utils/colors';
+const title = 'Продолжить';
+
+export default function Button() {
+  return (
+    <TouchableOpacity style={styles.button}>
+      <CustomText textType="SFProDisplayBlack" style={styles.title}>
+        {title}
+      </CustomText>
+    </TouchableOpacity>
+  );
+}
+const styles = StyleSheet.create({
+  button: {
+    justifyContent: 'center',
+
+    borderRadius: 16,
+    height: 52,
+    marginTop: 8,
+    marginBottom: 44,
+    backgroundColor: colors.MAIN_THEME_PURPLE,
+  },
+
+  title: {
+    color: colors.ACCENT_BUTTON_TEXT,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 10,
+  },
+});

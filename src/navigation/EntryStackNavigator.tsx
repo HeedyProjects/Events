ae26d62fee2dfa019b3312599595599720e38a19
Константин Двойнев <>
2022-07-06 23:53:49 +0300

    fix bug

--- src/navigation/EntryStackNavigator.tsx
+++ src/navigation/EntryStackNavigator.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import SignIn from '../screens/SignInScreen';
+import SignIn from '../screens/SignInScreen/Index';
 import Registration from '../screens/Registration/Index';
 
 const Stack = createNativeStackNavigator();

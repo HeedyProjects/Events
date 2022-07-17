c7f89daadd212e20d0582ab7ae833d85c16d10b2
Veronika <trotsenko_nika@mail.ru>
2022-07-17 19:53:37 +0200

    small changes

--- src/screens/Profile/Password.tsx
+++ src/screens/Profile/Password.tsx
@@ -16,6 +16,7 @@ export default function Password() {
           onChangeText={onChangeText}
           value={text}
           placeholder="example@com"
+          secureTextEntry={true}
         />
         <Eye style={styles.eye} />
       </View>
